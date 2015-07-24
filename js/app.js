38e060f4bb20bedc58b357aad04c403d856e2cae
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-07-23 23:24:03 -0400

    converted input field

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(event){ 
+
+//  $('.cart-icon').click(function(event){
+//  	$(this).addClass('cart-purchased-icon');
+//  	$('entered-item-text').addClass('purchased-item-text');
+//  	$('entered-item').addClass('purchased-item');
+// })
+
+
+
+});
