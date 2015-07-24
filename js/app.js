7e7b6bafc77672fa5d3db110e110a3d729f615ad
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-07-23 23:36:02 -0400

    notes made for jquery approach

--- js/app.js
+++ js/app.js
@@ -7,5 +7,21 @@ $(document).ready(function(event){
 // })
 
 
+//add a new item
+///on click of add-icon take inputs from text
+///create a new list item > p w/ text + container divs + associated buttons
+///insert (& animate) this new entered-item in the DOM --above ? the old items
+
+
+//mark an item as purchased
+///on click of cart-icon  animate/ change style of li > p and container div color
+///on click again of cart-icon change style back
+
+
+//delete an item
+///on click of delete icon remove the li > p, container divs, and sibling buttons from the DOM
+
+
 
 });
+
